57d70768f6eab61ab9807c1f8265c7714c81aa13
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-01-02 20:45:48 +0200

    Login page complete

--- client/src/Pages/LoginPage/LoginPage.js
+++ client/src/Pages/LoginPage/LoginPage.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import style from './LoginPage.module.scss';
+
+import Nav from '../../Components/Navigation/Navigation';
+
+import avo from '../../images/avo.jpg';
 
 function LoginPage() {
     return (
-        <div>
-            <h1>Hey from the Login Page</h1>
-        </div>
+        <>
+            <Nav />
+            <div style={{backgroundImage: `url(${avo})`}} className={style.main}>
+                <div className={style.container}>
+                    <div className={style.content}>
+                        <h1>Login</h1>
+                        <br />
+                        <br />
+                        <label htmlFor="name">Username Or Email</label>
+                        <br />
+                        <input id="name" type="text" placeholder='Enter Your Name...' />
+                        <br />
+                        <br />
+                        <label htmlFor="password">Password</label>
+                        <br />
+                        <input id='password' type="text" placeholder='Enter Your Password...' />
+                        <br /><br /><br />
+                        <button className={style.btn}>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
